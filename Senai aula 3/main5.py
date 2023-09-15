df7e0631c64bef3fc9ce147243afb1f2d4e06dd2
Tigas011 <144277779+Tigas011@users.noreply.github.com>
2023-09-15 14:55:19 -0300

    Update main5.py

--- Senai aula 3/main5.py
+++ Senai aula 3/main5.py
@@ -1,5 +1,6 @@
 # Declare uma variável contendo uma frase. Em seguida, peça ao usuário para digitar uma palavra e concatene essa palavra no final da frase. Exiba o resultado.
-frase = "A vida e uma caixa preta nunca saberemos o seu real "
-palavra = input("Digite uma palavra para terminar a frase:")
-print(frase + palavra)
+frase = "Mesmo que pareça difícil, "
+palavra = input("Digite uma palavra ")
+frase_pronta = frase + palavra
+print(frase_pronta)
 
