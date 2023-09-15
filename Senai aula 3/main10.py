b5f060e8e8dd3b421915aa0fc807e30fb17ee903
Tigas011 <144277779+Tigas011@users.noreply.github.com>
2023-09-15 16:44:05 -0300

    Update main10.py

--- Senai aula 3/main10.py
+++ Senai aula 3/main10.py
@@ -1,20 +1,20 @@
 # 1 - Crie uma condição para comparar idades: 45 e 18 -  QUAL É MENOR E QUAL É MAIOR?
-# idade1 = 45
-# idade2 = 18
-# if idade1 > idade2:
-#     maior = idade1
-#     menor = idade2
-# else:
-#     maior = idade2
-#     menor = idade1
-# print("Idade maior:", maior, "(ta quase aposentando em)")
-# print("Idade menor:", menor, "(ta na flor da pele meu jovem)")
+idade1 = 45
+idade2 = 18
+if idade1 > idade2:
+    maior = idade1
+    menor = idade2
+ else:
+     maior = idade2
+     menor = idade1
+ print("Idade maior:", maior, "(ta quase aposentando em)")
+ print("Idade menor:", menor, "(ta na flor da pele meu jovem)")
 
 
-# # 2 - Crie um sistema para permitir a verificação de menores em um show
-# idade = int(input("Digite sua idade "))
-# if idade < 18:
-#     print("Tá de menor ainda rapaz, volte para casa!")
+# 2 - Crie um sistema para permitir a verificação de menores em um show
+ idade = int(input("Digite sua idade "))
+ if idade < 18:
+     print("Tá de menor ainda rapaz, volte para casa!")
 
 # 3 - Crie um algoritmo que permita a entrada de 3 notas de alunos, utilize o bloco de código if() para verificar se o aluno passou.
 nota1 = int(input("Digite a nota do primeiro aluno "))
