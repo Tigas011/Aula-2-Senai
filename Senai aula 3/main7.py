02492b84a319de27cb579544d3b78621ea714e53
Tigas011 <144277779+Tigas011@users.noreply.github.com>
2023-09-15 16:13:55 -0300

    Update main7.py

--- Senai aula 3/main7.py
+++ Senai aula 3/main7.py
@@ -1,4 +1,7 @@
 # Declare duas variáveis com números de telefone, incluindo um código de área e o número principal. Concatene esses valores para formar um número de telefone completo.
-numero1 = "11 99009-"
-numero2 = "3546"
-print(numero1 + numero2)
+codigo_area = input("Digite o código de área: ")
+numero = input("Digite o número principal: ")
+
+numero_completo = f"+{codigo_area} {numero}"
+
+print(numero_completo)
