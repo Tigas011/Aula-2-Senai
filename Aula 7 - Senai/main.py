b6096d897d7b8429b099fb569d02ece02f3005c7
Tigas011 <144277779+Tigas011@users.noreply.github.com>
2023-09-22 14:39:03 -0300

    Update main.py

--- Aula 7 - Senai/main.py
+++ Aula 7 - Senai/main.py
@@ -1,55 +1,60 @@
-# def função_principal():
+def função_principal():
 # #  Bloco de código da função principal
-#     variavel1 = 10
-#     variavel2 = 20
+    variavel1 = 10
+    variavel2 = 20
 
-#     if variavel1 < variavel2:
-#     # Bloco de código do if
-#          print("Variavel 1 é menor que Variável 2")
-#     else:
-#     # Bloco de código do else
-#          print("Variavel 1 é maior ou igaul a Variável 2")
+    if variavel1 < variavel2:
+    # Bloco de código do if
+         print("Variavel 1 é menor que Variável 2")
+    else:
+    # Bloco de código do else
+         print("Variavel 1 é maior ou igaul a Variável 2")
 
-#     for i in range(5):
-#     #  Bloco de código do loop for
-#         print(f"Valor de i: {i}")
+    for i in range(5):
+    #  Bloco de código do loop for
+        print(f"Valor de i: {i}")
 
 # Fim do bloco de código da função principal
 # Fim do bloco de código do programa principal
 
 
-# função_principal()
+função_principal()
 
 # Estruturas de Repetição
 
-# c = 1
-# while c <= 10:
-#     print("contador", c)
-#     c = c + 3
+c = 1
+while c <= 10:
+    print("contador", c)
+    c = c + 3
 
-# Random == aleatorio
-# import random
-# aleatorio = random.random()
-# print(aleatorio)
-
-# # O método randint() da biblioteca random é usado para gerar um número inteiro aleatório dentro de um intervalo especificado.
-# x = random.randint(1,500)
-# print(x)
-
-
+Random == aleatorio
 import random
+aleatorio = random.random()
+print(aleatorio)
 
-print("Jogo de adivinhação, escolha um número e tente adivinhar")
+# O método randint() da biblioteca random é usado para gerar um número inteiro aleatório dentro de um intervalo especificado.
+x = random.randint(1,500)
+print(x)
 
-chute = input("Digite um número -> ")
 
-numero_aleatorio = random.randint(1, 2)
+ import random
 
-chute_int = int(chute)
+def adivinha_numero():
+    numero_aleatorio = random.randint(12, 25)
 
-if chute_int == numero_aleatorio:
-    print("Sensacional, vc acertou! O número é", numero_aleatorio)
-else:
-    print("Não foi dessa vez, vc errou! O número é", numero_aleatorio)
+    for tentativas in range(3):
+        chute = input("Digite um numero entre 12 e 25: ")
+
+        if chute == numero_aleatorio:
+            print("Você acertou!")
+            return
+
+        else:
+            print("Seu chute foi alto ou baixo.")
+
+    print("Você perdeu!")
+
+if __name__ == "__main__":
+    adivinha_numero()
 
 
