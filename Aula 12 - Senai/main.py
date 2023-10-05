ae952b6b53ea375c4ad363ea8a0ac25e02d53f39
Thiago Souza de Almeida <144277779+Tigas011@users.noreply.github.com>
2023-10-05 16:03:46 -0300

    Add files via upload

---
+++ Aula 12 - Senai/main.py
@@ -0,0 +1,52 @@
+# n = input("DIgite um número: ")
+# soma = lambda x:x+3
+# print(soma(10))
+
+# Objetivo - elevar a 3 todos os numeors da lista
+
+# lista = {1,2,3,6,2}
+# quadrado = list(map(lambda x: x ** 2, lista))
+# print(quadrado)
+
+# n = (10,20,30)
+# lista = [1,2,3,6,2]
+# quadrado2 = list(map(lambda numero:numero/12,lista))
+# print(quadrado2)
+# Multiplicação = lambda a,b: a*b
+# Print(multiplicação(10,20))
+
+
+# numeros = [200,20,1,30,0]
+
+# pares = list(filter(lambda x : x % 2 == 0, numeros))
+# print(pares)
+
+#                                                                    EXERCÍCIOS:
+
+# 1 - Crie uma função lambda que retorne o dobro de um número.
+
+# dobro = lambda x: 2 * x
+# print(dobro(1.5))
+
+# 2 - Crie uma função lambda que calcule a soma de dois números.
+
+# soma = lambda x, y: x + y
+# print(soma(5, 10))
+
+# 3 - Crie uma função lambda que verifique se um número é par.
+
+# par = lambda x: x % 2 == 0
+# print(par(2))
+
+# 4 - Crie uma função lambda que converta uma string em maiúsculas.
+
+# s_maiusculo = lambda s: s.upper()
+# print(s_maiusculo("ola mundo"))
+
+# 5 - Crie uma função lambda que calcule o produto de três números.
+
+# from functools import reduce
+# list = [4,8,7]
+# produto = reduce(lambda x,y: x * y, list)
+# print(produto)
+
