f17c007390e188ebe102bfd989ca97fceb526e04
Thiago Souza de Almeida <144277779+Tigas011@users.noreply.github.com>
2023-10-26 14:47:37 -0300

    Update untitled1.py

--- untitled1.py
+++ untitled1.py
@@ -1,4 +1,36 @@
-# -*- coding: utf-8 -*-
+# LINGUAGENS DE PROGRAMAÇÃO
+
+# A linguagem de programação é um método padronizado, formado por um conjunto de regras sintáticas e semânticas, de implementação
+# de um código fonte - que pode ser compilado e transformado em um programa de computador, ou usado como script interpretado - que informará instruções de processamento ao computador.
+
+# JAVA  -  C -  C++ - GOLANG -  PYTHON - JS …
+
+# A LINGUAGEM DA MÁQUINA É  0 E 1
+# COM AS LINGUAGENS DE PROGRAMAÇÃO PODEMOS TER UM INTERMÉDIO ENTRE A LINGUAGEM DE MÁQUINA E A LINGUAGEM HUMANA
+
+# Python é uma linguagem de programação de alto nível, interpretada, de propósito geral e de fácil leitura e escrita. 
+# Foi criada por Guido van Rossum e lançada pela primeira vez em 1991. 
+# Desde então, Python se tornou uma das linguagens mais populares e amplamente usadas no mundo da programação, sendo aplicada em uma variedade de domínios, desde desenvolvimento web até análise de dados e inteligência artificial.
+
+# Tipos de dados
+Dados são as informações a serem processadas por um computador
+
+# DADOS PRIMITIVOS:
+
+# Numéricos
+# Caracteres
+# Lógicos
+
+# Variáveis
+# Variáveis são símbolos usados na programação para armazenar e representar valores, permitindo manipulação e cálculos em um programa. Cada variável tem um nome único e um tipo que define o tipo de valor que pode ser armazenado nela.
+
+# Nomenclatura de variáveis:
+# Um nome de variável é uma sequência de letras (a → z, A → Z) e números (0 → 9), que devem sempre começar com uma letra. Apenas letras comuns são permitidas.
+# Letras acentuadas, cedilhas, espaços, caracteres especiais como $, #, @, etc. são proibidos, exceto para o caractere _ (sublinhado/underline).
+
+
+
+
 num1 = float(input("Digite o primeiro número: "))
 num2 = float(input("Digite o segundo número: "))
 
