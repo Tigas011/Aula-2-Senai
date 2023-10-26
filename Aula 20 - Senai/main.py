99fabe32654c2255bf5a66e19f88ebabbabc0776
Thiago Souza de Almeida <144277779+Tigas011@users.noreply.github.com>
2023-10-26 14:10:45 -0300

    Add files via upload

---
+++ Aula 20 - Senai/main.py
@@ -0,0 +1,31 @@
+# # SISTEMA DE GERENCIAMENTO DE PACOTES -  PIP
+
+# O `pip` é um sistema de gerenciamento de pacotes para Python. Ele é uma ferramenta que facilita a instalação, atualização e gerenciamento de bibliotecas e pacotes Python de terceiros. O nome "pip" é uma sigla para "Pip Installs Packages" (ou "Pip Instala Pacotes", em português), o que reflete sua principal função: instalar pacotes Python.
+
+# Algumas das principais funcionalidades do `pip` incluem:
+
+# 1. **Instalação de Pacotes:** O `pip` permite que você instale pacotes Python diretamente do Python Package Index (PyPI) e de outros repositórios de pacotes. Você pode instalar bibliotecas e ferramentas de terceiros com facilidade usando um simples comando `pip install`.
+# 2. **Atualização de Pacotes:** Com o `pip`, você pode atualizar pacotes Python para suas versões mais recentes com o comando `pip install --upgrade`.
+# 3. **Remoção de Pacotes:** O `pip` também permite a remoção de pacotes que você não precisa mais, com o comando `pip uninstall`.
+# 4. **Listagem de Pacotes Instalados:** Você pode listar todos os pacotes Python instalados em seu ambiente com o comando `pip list`.
+# 5. **Requisitos e Ambientes Virtuais:** O `pip` é comumente usado em conjunto com arquivos de requisitos (`requirements.txt`) para especificar as dependências de um projeto. Além disso, ele é usado em ambientes virtuais para isolar as dependências de diferentes projetos.
+
+# COMO INSTALAR UMA BIBLIOTECA EM PYTHON
+
+# Abra o Terminal (Linux/macOS) ou o Prompt de Comando (Windows): 
+# Você precisará acessar um terminal ou prompt de comando para executar os comandos.
+
+# Use o Comando pip install: Para instalar uma biblioteca, use o seguinte comando 
+# pip install seguido pelo nome da biblioteca que você deseja instalar. Por exemplo, 
+# se você quiser instalar a biblioteca requests, execute o seguinte comando:
+
+# **pip install NOME DA BIBLIOTECA**
+
+# Isso baixará e instalará a biblioteca 
+
+# Aguarde a Instalação: O pip fará o download da biblioteca 
+# e suas dependências, 
+# se houver, e as instalará em seu ambiente Python. 
+# Aguarde até que o processo seja 
+# concluído.
+
